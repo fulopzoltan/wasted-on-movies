4810dba94f568ff81e32f5ec9dd013420bc169d2
Fulop Zoltan <fulopzoltan20@gmail.com>
2022-06-12 21:28:30 +0300

    firebase url update

--- src/utils/FirebaseAPI.ts
+++ src/utils/FirebaseAPI.ts
@@ -8,7 +8,7 @@ class FirebaseAPI {
     private baseURL: string;
 
     constructor() {
-        this.baseURL = 'http://localhost:5001/wasted-on-movies/europe-west3/api' || FIREBASE_FUNC_BASE_URL || '';
+        this.baseURL = FIREBASE_FUNC_BASE_URL || '';
         this.client = this.createAxiosClient();
     }
     private createAxiosClient(): AxiosInstance {
